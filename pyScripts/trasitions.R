f88f86182c04b771386b94c54bf7cb20c73f1f4f
Sarah Urbut <sarahmurbut@gmail.com>
2025-10-27 07:05:10 -0500

    Update

--- pyScripts/trasitions.R
+++ pyScripts/trasitions.R
@@ -11,6 +11,17 @@ colnames(E)=all_patient_diseases$X0
 ## identify patients who had myocardial infarction before age 51
 sick=E[E[,"Myocardial infarction"]<51,]
 
-## column by column, idenfity number of people with diseases that occurred before myocardial infarction
+## column by column, idenfity number of people with MI who had anohter disease that occurred before myocardial infarction
 pre_disease=apply(sick,2,function(x){sum(x<sick[,"Myocardial infarction"])})
+pop_norm_pre_disease=pre_disease/colSums(E)
 pre_disease_enrichment=pre_disease/colSums(sick<51)
+pre_disease_df=data.frame(disease=names(pre_disease),count=pre_disease,
+                              pop_norm=pop_norm_pre_disease,
+                              enrichment=pre_disease_enrichment)
+
+## for age matched population, count number of people with a given disease not conditional on having MI
+## 
+## age_matched_matrix=matrix(NA,nrow=nrow(sick),ncol=ncol(sick))
+## for(i in 1:nrow(sick)){  
+## sick_age=sick[i,"Myocardial infarction"]
+## 
